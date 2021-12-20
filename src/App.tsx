535bd3803c915b96ff2c61f7db7f711315a98679
Jaehyeok <milkboy2564@naver.com>
2021-12-21 06:49:50 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Start = styled.div`
+  font-size: 100px;
+`;
+
+function App() {
+  return <Start>Hello</Start>;
+}
+
+export default App;
